8d62e26f06724132676bd42be06731f62928fc24
Henrico1234 <henricocostta@gmail.com>
2023-10-18 15:52:33 -0300

    atu

--- aritimetica.c
+++ aritimetica.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "aritimetica.h"
 
 
@@ -7,12 +8,36 @@ void aritimetica(char aux[][100], char instrucao[], char op1[], char op2[], char
 int r[32][5];
 int i;
 if(strcmp(instrucao,"ADD") == 0){
-    for(i =0 ; i < 5; i++){
-        r;
-    }
-
-
- printf("%s %s %s", op1, op2, op3);
-
+        op1[0] = '0';
+        op2[0] = '0';
+        op3[0] = '0';
+        r[atoi(op1)] = r[atoi(op2)] + r[atoi(op3)];
+}
+if(strcmp(instrucao,"SUB") == 0){
+        op1[0] = '0';
+        op2[0] = '0';
+        op3[0] = '0';
+        r[atoi(op1)] = r[atoi(op2)] - r[atoi(op3)];
+}
+if(strcmp(instrucao,"MUL") == 0){
+        op1[0] = '0';
+        op2[0] = '0';
+        op3[0] = '0';
+        r[atoi(op1)] = r[atoi(op2)] * r[atoi(op3)];
+}
+if(strcmp(instrucao,"DIV") == 0){
+        op1[0] = '0';
+        op2[0] = '0';
+        op3[0] = '0';
+        r[atoi(op1)] = r[atoi(op2)] / r[atoi(op3)];
+}
+if(strcmp(instrucao,"MOD") == 0){
+        op1[0] = '0';
+        op2[0] = '0';
+        op3[0] = '0';
+        r[atoi(op1)] = r[atoi(op2)] % r[atoi(op3)];
+        if(r[atoi(op1)] < 0){
+            r[atoi(op1)] = r[atoi(op1)] + r[atoi(op3)];
+        }
 }
 }
--- decodifica.c
+++ decodifica.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "decodifica.h"
 #include "aritimetica.h"
 
@@ -7,10 +8,18 @@
 void decodifica(char aux[][100], int N){
     int i;
     char instrucao[15], op1[15], op2[15], op3[15];
+    int r[32];
 
     for (i = 0 ; i < N; i++)
     {
+
         sscanf(aux[i], "%s", instrucao);
+        if(strcmp(instrucao,"MOV") == 0){
+            sscanf(aux[i], "%s %s %s", instrucao, op1, op2);
+            r[i] = r[op1] 
+            
+
+        }
         if(strcmp(instrucao,"ADD") == 0 ||strcmp(instrucao,"SUB") == 0 || strcmp(instrucao,"MUL") == 0 || strcmp(instrucao,"DIV") == 0 ||strcmp(instrucao,"MOD") == 0){
             sscanf(aux[i], "%s %s %s %s", instrucao, op1, op2, op3);
             aritimetica(aux, instrucao, op1, op2, op3);  
